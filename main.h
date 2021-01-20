7c91e5a161c5847384dc6c1dd236d58e20d41a8b
YuTecHealth <service@yutechealth.com>
2021-01-20 11:35:03 +0800

    Initial commit of git tutor repos

---
+++ main.h
@@ -0,0 +1,9 @@
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ble.h"
+#include "ble_srv_common.h"
+#include "nrf_sdh_ble.h"
+
+uint32_t ble_button2_send(uint16_t conn_handle, ble_lbs_t * p_lbs, uint8_t val);
